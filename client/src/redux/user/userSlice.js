5007614d3bfdc6d1e55f9ad3683495bab371e6ac
Saad Khan <saad.khan955@gmail.com>
2024-01-30 18:42:37 +0530

    Added redux toolkit

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+}
+
+const userSlices = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false
+    }
+  }
+})
+
+export const { signInStart, signInSuccess, signInFailure } = userSlices.actions
+export default userSlices.reducer;
